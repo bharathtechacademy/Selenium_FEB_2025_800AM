0c3976933776ab1e7a52e0c78b528292de068e2d
bharathautomation <soft.ittech20@gmail.com>
2025-03-07 08:46:08 +0530

    collections

--- src/com/creatio/crm/java/basics/CollectionsConcepts.java
+++ src/com/creatio/crm/java/basics/CollectionsConcepts.java
@@ -1,11 +1,16 @@
 package com.creatio.crm.java.basics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class CollectionsConcepts {
@@ -63,7 +68,7 @@ public class CollectionsConcepts {
 		//ArrayList follows Insertion order
 		//ArrayList allow null values
 		
-		List<Integer> empIdsArrayList = new ArrayList<Integer>();
+		List<Object> empIdsArrayList = new ArrayList<>();
 		List<String> empNamesArrayList = new ArrayList<String>();
 		empNamesArrayList.add("Ashok");
 		empNamesArrayList.add("Maha");
@@ -179,6 +184,143 @@ public class CollectionsConcepts {
 		
 		System.out.println("Size of TreeSet is :"+empNamesTreeSet.size());
 		System.out.println(empNamesTreeSet);
+		
+		
+		/*********************HashMap*************************/
+		System.out.println("*********************HashMap*************************");
+		
+		//Syntax to Initialize: Collection <Key,Value> variable = new Collection<Key,Value>();
+		//Syntax to add data: variable.put(key, value);
+		//Syntax to remove the data:variable.remove(key);
+		//Syntax to get the total value : variable.size();
+		
+		//Order of Storing : Random Order
+		//Null Values : Allowed	
+		//Null Keys : Allowed
+
+		Map<String, Integer> empNamesHashMap = new HashMap<String, Integer>();
+		empNamesHashMap.put("Ashok",1);
+		empNamesHashMap.put("Bharath",2);
+		empNamesHashMap.put("Ashok",3);
+		empNamesHashMap.put("Maha",4);
+		empNamesHashMap.put("Ajay",5);
+		empNamesHashMap.put("Gogu",null);
+		empNamesHashMap.put(null,7);
+		empNamesHashMap.remove("Bharath");
+		
+		System.out.println("Size of HashMap is :"+empNamesHashMap.size());
+		System.out.println(empNamesHashMap.get(5));
+		System.out.println(empNamesHashMap);
+		
+//		Map<Integer,String> empNamesHashMap = new HashMap<Integer,String>();
+//		empNamesHashMap.put(1,"Ashok");
+//		empNamesHashMap.put(2,"Bharath");
+//		empNamesHashMap.put(3,"Ashok");
+//		empNamesHashMap.put(1,"Maha");
+//		empNamesHashMap.put(5,"Ajay");
+//		empNamesHashMap.put(6,null);
+//		empNamesHashMap.put(null,"Charan");
+//		empNamesHashMap.remove(2);
+//		
+//		System.out.println("Size of HashMap is :"+empNamesHashMap.size());
+//		System.out.println(empNamesHashMap.get(5));
+//		System.out.println(empNamesHashMap);
+		
+		/*********************LinkedHashMap*************************/
+		System.out.println("*********************LinkedHashMap*************************");
+		
+		//Syntax to Initialize: Collection <Key,Value> variable = new Collection<Key,Value>();
+		//Syntax to add data: variable.put(key, value);
+		//Syntax to remove the data:variable.remove(key);
+		//Syntax to get the total value : variable.size();
+		
+		//Order of Storing : Insertion Order
+		//Null Values : Allowed	
+		//Null Keys : Allowed
+
+		Map<String, Integer> empNamesLinkedHashMap = new LinkedHashMap<String, Integer>();
+		empNamesLinkedHashMap.put("Ashok",1);
+		empNamesLinkedHashMap.put("Bharath",2);
+		empNamesLinkedHashMap.put("Ashok",3);
+		empNamesLinkedHashMap.put("Maha",4);
+		empNamesLinkedHashMap.put("Ajay",5);
+		empNamesLinkedHashMap.put("Gogu",null);
+		empNamesLinkedHashMap.put(null,7);
+		empNamesLinkedHashMap.remove("Bharath");
+		
+		System.out.println("Size of LinkedHashMap is :"+empNamesLinkedHashMap.size());
+		System.out.println(empNamesLinkedHashMap.get(5));
+		System.out.println(empNamesLinkedHashMap);
+		
+		/*********************TreeMap*************************/
+		System.out.println("*********************TreeMap*************************");
+		
+		//Syntax to Initialize: Collection <Key,Value> variable = new Collection<Key,Value>();
+		//Syntax to add data: variable.put(key, value);
+		//Syntax to remove the data:variable.remove(key);
+		//Syntax to get the total value : variable.size();
+		
+		//Order of Storing : Ascending Order
+		//Null Values : Allowed	
+		//Null Keys : Not Allowed
+
+		Map<String, Integer> empNamesTreeMap = new TreeMap<String, Integer>();
+		empNamesTreeMap.put("Ashok",1);
+		empNamesTreeMap.put("Bharath",2);
+		empNamesTreeMap.put("Ashok",3);
+		empNamesTreeMap.put("Maha",4);
+		empNamesTreeMap.put("Ajay",5);
+		empNamesTreeMap.put("Gogu",null);
+//		empNamesTreeMap.put(null,7);
+		empNamesTreeMap.remove("Bharath");
+		
+		System.out.println("Size of TreeMap is :"+empNamesTreeMap.size());
+		System.out.println(empNamesTreeMap);
+		
+		
+		/*********************Hashtable*************************/
+		System.out.println("*********************Hashtable*************************");
+		
+		//Syntax to Initialize: Collection <Key,Value> variable = new Collection<Key,Value>();
+		//Syntax to add data: variable.put(key, value);
+		//Syntax to remove the data:variable.remove(key);
+		//Syntax to get the total value : variable.size();
+		
+		//Order of Storing : Random Order
+		//Null Values : Not Allowed	
+		//Null Keys : Not Allowed
+
+		Map<String, Integer> empNamesHashtable = new Hashtable<String, Integer>();
+		empNamesHashtable.put("Ashok",1);
+		empNamesHashtable.put("Bharath",2);
+		empNamesHashtable.put("Ashok",3);
+		empNamesHashtable.put("Maha",4);
+		empNamesHashtable.put("Ajay",5);
+		empNamesHashtable.put("Charan",6);
+//		empNamesHashtable.put("Gogu",null);
+//		empNamesHashtable.put(null,7);
+		empNamesHashtable.remove("Bharath");
+		
+		System.out.println("Size of Hashtable is :"+empNamesHashtable.size());
+		System.out.println(empNamesHashtable);
+		
+		
+		
+		HashMap<String,Object> data1 = new HashMap<String,Object>();
+		data1.put("Name", "Bharath Reddy");
+		data1.put("Id", 1234);
+		data1.put("VisaStatus", true);
+		
+		HashMap<String,Object> data2 = new HashMap<String,Object>();
+		data2.put("Name", "Charan Reddy");
+		data2.put("Id", 1235);
+		data2.put("VisaStatus", true);
+		
+		List<Map<String,Object>> dataSet = new ArrayList<Map<String,Object>> ();
+		dataSet.add(data1);
+		dataSet.add(data2);
+		
+		System.out.println(dataSet.get(1).get("Name"));
 		
 
 	}
